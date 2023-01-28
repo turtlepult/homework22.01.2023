42486e80d5a13e6bd5f27aea8c068196fad70a38
Ivan_Ladygin <ivan_ladiygin@mail.ru>
2023-01-28 18:25:38 +0500

    perhaps a working program has been created

---
+++ HW002/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+int[] array = new int[10];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (i%2!=0)
+    {
+        Console.WriteLine(array[i]+" ");
+    }
+}
